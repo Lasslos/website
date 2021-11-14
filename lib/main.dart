3de2327d2e13c4670f3d9ef54f31c54a0bdab6b3
Lasslos <81803114+Lasslos@users.noreply.github.com>
2021-11-13 17:54:18 -0700

    Reset
    I was stuck and didn't have fun anymore so here we go.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:vrouter/vrouter.dart';
-import 'package:dynamic_theme_mode/dynamic_theme_mode.dart';
-import 'package:website/constants/style.dart' as style;
-import 'package:website/routing/my_routes.dart';
+import 'package:website/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,46 +10,7 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => DynamicThemeMode(
-    initialThemeMode: ThemeMode.system,
-    builder: (BuildContext context, ThemeMode themeMode) {
-      return VRouter(
-        debugShowCheckedModeBanner: false,
-        title: 'Canada - Laslo Hauschild',
-        theme: _buildTheme(Brightness.light),
-        darkTheme: _buildTheme(Brightness.dark),
-        themeMode: themeMode,
-        routes: myRoutes,
-      );
-    },
+  Widget build(BuildContext context) => const MaterialApp(
+    home: HomePage(),
   );
-
-  ThemeData _buildTheme(Brightness brightness) {
-    var brightnessColor = style.getBackgroundColor(brightness: brightness);
-    var antiBrightnessColor = style.getOnBackgroundColor(brightness: brightness);
-    var systemUiOverlayStyle = brightness == Brightness.light
-        ? SystemUiOverlayStyle.light
-        : SystemUiOverlayStyle.dark;
-
-    return ThemeData(
-      brightness: brightness,
-      primarySwatch: Colors.green,
-      colorScheme: ColorScheme.fromSwatch(
-        primarySwatch: Colors.green,
-        primaryColorDark: Colors.green,
-        accentColor: Colors.redAccent,
-        brightness: brightness,
-        backgroundColor: brightnessColor,
-      ),
-      textTheme: GoogleFonts.notoSansTextTheme(const TextTheme()),
-      appBarTheme: AppBarTheme(
-        systemOverlayStyle: systemUiOverlayStyle,
-        backgroundColor: brightnessColor,
-        foregroundColor: antiBrightnessColor,
-        toolbarTextStyle: const TextStyle(color: style.gray),
-        titleTextStyle: const TextStyle(),
-        iconTheme: const IconThemeData(color: Colors.green),
-      ),
-    );
-  }
 }
